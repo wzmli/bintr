6fda3f863507caf00413cee12851d1bdec14d583
wzmli <wzmichael.li@gmail.com>
2022-07-20 13:54:13 -0400

    Autocommit

--- fit.R
+++ fit.R
@@ -3,7 +3,7 @@ library(bbmle)
 library(shellpipes)
 loadEnvironments()
 
-init_lK <- 5000
+init_lK <- 100
 
 peak <- (fulldf
 	%>% filter(Inc == max(Inc))
@@ -11,12 +11,14 @@ peak <- (fulldf
 
 fitdf <- (fulldf
 	%>% filter(t <= peak[["t"]])
+	%>% rename(time = t)
+	%>% mutate(Inc = round(Inc)) ## Do I really need this?!?
 )
 
 print(fitdf,n=Inf)
 
 m <- mle2(fitdf$Inc ~ dnbinom(mu=flexSim(lK, li0,rsim,time)$intSim, size=ss)
-	, start = list(lK=log(init_lK),li0=1,rsim=0.05,ss=1)
+	, start = list(lK=log(init_lK),li0=1,rsim=0.2,ss=1)
 	, data = fitdf
 )
 
